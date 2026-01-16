1f4dc1640cafa8705a66804a0b0659c29c054782
IhitplayzYT <ihitexe@gmail.com>
2026-01-16 19:27:18 +0530

    Tyding up main and also finished all required string matcher functions

--- main.go
+++ main.go
@@ -23,11 +23,6 @@ const False bool = false
 // Placeholder
 var website_list []string = []string{"a", "b", "c"}
 
-//  abc
-// cabd
-// 0123
-// i = 3,cur_idx = 2
-
 func match_longest_any(to_be_found, to_be_searched string) (int, int) {
 	lf, ls := len(to_be_found), len(to_be_searched)
 	mx_len, ed_idx := 0, -1
@@ -52,9 +47,30 @@ func match_longest_any(to_be_found, to_be_searched string) (int, int) {
 	return ed_idx - mx_len, mx_len
 }
 
-// :TODO
-func match_longest(to_be_found, to_be_searched string) (int, int) {
+func match_longest_prefix(to_be_found, to_be_searched string) (int, int) {
+	cur_idx, mx_len, mx_idx, lf, ls := 0, 0, -1, len(to_be_found), len(to_be_searched)
+	for i := 0; i < ls; i++ {
+		if cur_idx == lf {
+			return i - lf, lf
+		}
+		if to_be_found[cur_idx] == to_be_searched[i] {
+			cur_idx += 1
+		} else {
+			if mx_len < cur_idx {
+				mx_len = cur_idx
+				mx_idx = i - cur_idx
+			}
 
+			if to_be_found[0] == to_be_searched[i] {
+				cur_idx = 1
+			} else {
+				cur_idx = 0
+			}
+
+		}
+
+	}
+	return mx_idx, mx_len
 }
 
 // CAN FAIL WITH -1
@@ -75,47 +91,63 @@ func match_full(to_be_found, to_be_searched string) int {
 }
 
 func usage(str string) {
-	fmt.Println(str, " [-d] <KEYWORD>")
+	fmt.Println("Usage : ", str, " [-d] <KEYWORD>")
 	return
 }
 
 func print_map(s map[string][]string) {
 	for k, v := range s {
 		fmt.Print("[%v]:", k)
-		z := len(k)
-		for i := 0; i < len(v); i++ {
+		z, y := len(k), len(v)
+		for i := 0; i < y; i++ {
 			fmt.Println(strings.Repeat(" ", z+2), v[i])
 		}
+		fmt.Println()
 	}
 }
 
+// :TODO
+func get_html(link string) []string {
+	var ret []string
+	return ret
+}
+
+// :TODO
 func search(str string, flag bool) map[string][]string {
 	ret := make(map[string][]string)
 	for _, v := range website_list {
+		html := get_html(v)
 
 	}
 
 	return ret
 }
 
-func main() {
-	args := os.Args
+func parse_args(args []string) (string, bool) {
 	keyword, deep_flag := "", false
 	l := len(args)
 	if l < 2 || l > 3 {
 		usage(args[0])
-		return
+		return "", false
 	} else if l == 2 {
 		keyword = args[1]
 	} else {
 		if args[1] != "-d" {
 			usage(args[0])
-			return
+			return "", false
 		}
 		deep_flag = true
 		keyword = args[2]
 	}
+	return keyword, deep_flag
+}
 
+func main() {
+	args := os.Args
+	keyword, deep_flag := parse_args(args)
+	if keyword == "" {
+		return
+	}
 	ret := search(keyword, deep_flag)
 	print_map(ret)
 	return
