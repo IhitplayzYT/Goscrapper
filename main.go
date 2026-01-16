1d6a470013cc8ab2b7f2dbd07f27d22fd0466741
IhitplayzYT <ihitexe@gmail.com>
2026-01-16 18:56:04 +0530

    Added some matching/search fxn to find substrings or match whole strings

--- main.go
+++ main.go
@@ -2,14 +2,77 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"strings"
 )
 
+type i8 int8
+type i16 int16
+type i32 int32
+type i64 int64
+type s8 uint8
+type s16 uint16
+type s32 uint32
+type s64 uint64
+type f32 float32
+type f64 float64
+
+const True bool = true
+const False bool = false
+
 // Placeholder
 var website_list []string = []string{"a", "b", "c"}
+
+//  abc
+// cabd
+// 0123
+// i = 3,cur_idx = 2
+
+func match_longest_any(to_be_found, to_be_searched string) (int, int) {
+	lf, ls := len(to_be_found), len(to_be_searched)
+	mx_len, ed_idx := 0, -1
+	dp := make([][]int, lf+1)
+	for i := 0; i <= lf; i++ {
+		dp[i] = make([]int, ls+1)
+	}
+
+	for i := 1; i <= lf; i++ {
+		for j := 1; j <= ls; j++ {
+			if to_be_found[i-1] == to_be_searched[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+				if dp[i][j] > mx_len {
+					mx_len = dp[i][j]
+					ed_idx = j
+				}
+			} else {
+				dp[i][j] = dp[i-1][j-1]
+			}
+		}
+	}
+	return ed_idx - mx_len, mx_len
+}
+
+// :TODO
+func match_longest(to_be_found, to_be_searched string) (int, int) {
+
+}
+
+// CAN FAIL WITH -1
+func match_full(to_be_found, to_be_searched string) int {
+	ls := len(to_be_searched)
+	lf := len(to_be_found)
+	idx := 0
+	for i := 0; i < ls; i++ {
+		if to_be_found[idx] == to_be_searched[i] {
+			idx += 1
+		}
+		if idx == lf {
+			return i - lf + 1
+		}
+	}
+
+	return -1
+}
 
 func usage(str string) {
 	fmt.Println(str, " [-d] <KEYWORD>")
@@ -29,14 +92,9 @@ func print_map(s map[string][]string) {
 func search(str string, flag bool) map[string][]string {
 	ret := make(map[string][]string)
 	for _, v := range website_list {
-		res, err := http.Get(v)
-		html_ret, err := io.ReadAll(res.Body)
-		res.Body.Close()
-		if err != nil {
-			ret[v] = append(ret[v], html_ret)
-		}
 
 	}
+
 	return ret
 }
 
